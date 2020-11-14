022f3baf5be76dbd38407abc6ce72371c75bd9a0
Poppy Yasuko <pyasuko@un-a3.kzn.21-school.ru>
2020-11-14 15:39:19 +0300

    14.11

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: pyasuko <pyasuko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 15:26:42 by pyasuko           #+#    #+#             */
-/*   Updated: 2020/11/09 19:03:58 by pyasuko          ###   ########.fr       */
+/*   Updated: 2020/11/14 15:27:21 by pyasuko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,11 @@ char	*ft_strdup(const char *s1)
 	size_t			len;
 	char			*res;
 
-	len = ft_strlen(s1) + 1;
-	res = malloc(len);
+	len = ft_strlen(s1);
+	res = malloc(len + 1);
 	if (!res)
 		return (NULL);
 	else
-		ft_strlcpy(res, s1, len);
-	res[len] = '\0';
+		ft_strlcpy(res, s1, len + 1);
 	return (res);
 }
